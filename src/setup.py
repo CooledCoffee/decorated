22fabe935a57d67b599806443ce74e669fbd0a59
CooledCoffee <limc@smvp.cn>
2013-05-20 07:35:37 +0800

    increase package version

--- src/setup.py
+++ src/setup.py
@@ -3,9 +3,18 @@ from distutils.core import setup
 
 setup(
     name='decorated',
-    version='1.0.1',
+    version='1.0.2',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
     description='Base components for easily writing decorators.',
     packages=['decorated'],
     url='https://github.com/CooledCoffee/decorated/',
