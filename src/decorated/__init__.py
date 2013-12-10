81f326fbd0ae04f160cf73e7af682a80a5cabff4
CooledCoffee <CooledCoffee@gmail.com>
2013-12-10 14:48:47 +0800

    export Event in decorated package

--- src/decorated/__init__.py
+++ src/decorated/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from decorated.function import Function, BoundedFunction, PartialFunction
 from decorated.util.conditional import Conditional
+from decorated.util.events import Event
 from decorated.util.remove_extra_args import RemoveExtraArgs
 from decorated.util.retries import Retries
 
@@ -11,3 +12,4 @@ BoundedFunction = BoundedFunction
 retries = Retries
 conditional = Conditional
 remove_extra_args = RemoveExtraArgs
+Event = Event
