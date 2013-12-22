54e982f53c6ebdea3fa90f7004ae38b4d0b6d153
CooledCoffee <CooledCoffee@gmail.com>
2013-12-22 10:06:06 +0800

    version 1.2.4

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='decorated',
-    version='1.2.3',
+    version='1.2.4',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
