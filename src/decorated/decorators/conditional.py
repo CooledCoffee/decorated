442c555009160a9f45cde04b4ae8bdb0d27c311e
CooledCoffee <CooledCoffee@gmail.com>
2013-12-26 14:33:29 +0800

    method support in @conditional

--- src/decorated/decorators/conditional.py
+++ src/decorated/decorators/conditional.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
 from decorated.base.function import Function
+from decorated.decorators.remove_extra_args import RemoveExtraArgs
 import six
 
 class Conditional(Function):
     def _call(self, *args, **kw):
-        func_kw = self._resolve_args(*args, **kw)
-        if isinstance(self._condition, six.string_types):
-            condition = eval(self._condition, func_kw)
-        else:
-            condition_kw = self._condition._resolve_args(**func_kw)
-            condition = self._condition(**condition_kw)
-        if condition:
+        if self._test_condition(*args, **kw):
             return super(Conditional, self)._call(*args, **kw)
-
-    def _condition(self):
-        return True
-    
-    def _init(self, condition=None):
-        if condition:
-            if isinstance(condition, six.string_types):
-                self._condition = condition
-            elif callable(condition):
-                self._condition = Function(condition)
-            else:
-                raise Exception('Condition can only be string or callable.')
+        
+    def _init(self, condition):
+        if isinstance(condition, six.string_types):
+            self._condition = condition
+        elif callable(condition):
+            self._condition = RemoveExtraArgs(condition)
         else:
-            self._condition = Function(self._condition)
+            raise Exception('Condition can only be string or callable.')
+        
+    def _test_condition(self, *args, **kw):
+        d = self._resolve_args(*args, **kw)
+        if 'self' in d:
+            d['self_'] = d.pop('self')
+        if isinstance(self._condition, six.string_types):
+            return eval(self._condition, d)
+        else:
+            return self._condition(**d)
         
--- test/decorators_test/conditional_test.py
+++ test/decorators_test/conditional_test.py
@@ -3,23 +3,6 @@ from decorated.decorators.conditional import Conditional
 from unittest.case import TestCase
 
 class ConditionalTest(TestCase):
-    def test_no_condition(self):
-        @Conditional
-        def foo(a, b):
-            return a
-        self.assertEqual(1, foo(1, 1))
-        self.assertEqual(2, foo(2, 2))
-        
-    def test_method(self):
-        class TestConditional(Conditional):
-            def _condition(self, a):
-                return a == 1
-        @TestConditional
-        def foo(a, b):
-            return a
-        self.assertEqual(1, foo(1, 1))
-        self.assertIsNone(foo(2, 2))
-        
     def test_string(self):
         @Conditional(condition='a == 1')
         def foo(a, b):
@@ -42,4 +25,13 @@ class ConditionalTest(TestCase):
             return a
         self.assertEqual(1, foo(1, 1))
         self.assertIsNone(foo(2, 2))
+        
+    def test_on_method(self):
+        class Foo(object):
+            @Conditional(condition=lambda a: a == 1)
+            def bar(self, a):
+                return a
+        foo = Foo()
+        self.assertEqual(1, foo.bar(1))
+        self.assertIsNone(foo.bar(0))
         
