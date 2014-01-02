3aea012e43d39efe626ac263612e2ba082786f53
CooledCoffee <CooledCoffee@gmail.com>
2014-01-02 15:15:01 +0800

    use single instance for @instantiate

--- src/decorated/decorators/instantiate.py
+++ src/decorated/decorators/instantiate.py
@@ -4,13 +4,12 @@ from decorated.base.function import Function
 class Instantiate(Function):
     def _decorate(self, cls):
         self.class_ = cls
+        instance = cls()
+        method = getattr(instance, self._method)
         def _func(*args, **kw):
-            obj = cls()
-            method = getattr(obj, self._method)
             return method(*args, **kw)
         _func.__name__ = cls.__name__
         _func.__module__ = cls.__module__
-        _func.__doc__ = cls.__doc__
         return super(Instantiate, self)._decorate(_func)
     
     def _init(self, method='__call__'):
--- test/decorators_test/instantiate_test.py
+++ test/decorators_test/instantiate_test.py
@@ -9,6 +9,8 @@ class InstantiateTest(TestCase):
         class Foo(object):
             def __call__(self, a, b):
                 return a + b
+        self.assertEqual('Foo', Foo.__name__)
+        self.assertEqual('decorators_test.instantiate_test', Foo.__module__)
         self.assertTrue(inspect.isclass(Foo.class_))
         result = Foo(1, b=2)
         self.assertEqual(3, result)
@@ -16,6 +18,17 @@ class InstantiateTest(TestCase):
     def test_custom_call(self):
         @Instantiate('run')
         class Foo(object):
+            def run(self, a, b):
+                return a + b
+        result = Foo(1, b=2)
+        self.assertEqual(3, result)
+        
+    def test_callable(self):
+        @Instantiate('run')
+        class Foo(object):
+            def __call__(self, a, b):
+                return a * b
+            
             def run(self, a, b):
                 return a + b
         result = Foo(1, b=2)
