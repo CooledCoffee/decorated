430864a42457289dbc8733dc262f35d0589f1975
CooledCoffee <CooledCoffee@gmail.com>
2014-01-02 14:56:33 +0800

    file lock

--- src/decorated/decorators/synchronized.py
+++ src/decorated/decorators/synchronized.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from decorated.base.function import Function
 from threading import RLock
+import fcntl
+import os
 
 class Synchronized(Function):
     def _init(self, lock):
@@ -11,3 +13,24 @@ class Synchronized(Function):
             return super(Synchronized, self)._call(*args, **kw)
 
 MemoryLock = RLock
+
+class FileLock(object):
+    def __init__(self, path):
+        self._path = path
+        
+    def __enter__(self):
+        _create_file_if_not_exist(self._path)
+        self._fd = os.open(self._path, os.O_RDWR)
+        fcntl.flock(self._fd, fcntl.LOCK_EX)
+        return self
+    
+    def __exit__(self, t, v, tb):
+        fcntl.flock(self._fd, fcntl.LOCK_UN)
+        
+def _create_file_if_not_exist(path):
+    try:
+        f = open(path, 'w')
+        f.close()
+    except OSError:
+        pass
+    
--- test/decorators_test/synchronized_test.py
+++ test/decorators_test/synchronized_test.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
-from decorated.decorators.synchronized import MemoryLock, Synchronized
+from decorated.decorators.synchronized import MemoryLock, Synchronized, FileLock
 from threading import Thread
 from unittest.case import TestCase
 import time
+
+class FileLockTest(TestCase):
+    def test(self):
+        # set up
+        def foo():
+            with FileLock('/tmp/lock'):
+                time.sleep(0.01)
+        class BlockThread(Thread):
+            def run(self):
+                foo()
+                
+        # test
+        thread1 = BlockThread()
+        thread2 = BlockThread()
+        begin = time.time()
+        thread1.start()
+        thread2.start()
+        thread1.join()
+        thread2.join()
+        end = time.time()
+        self.assertGreater(end - begin, 0.02)
 
 class SynchronizedTest(TestCase):
     def test_simple(self):
