701f255a39a40915942a98b658b949a6b0415ee7
CooledCoffee <CooledCoffee@gmail.com>
2014-03-17 12:28:49 +0800

    more tests

--- test/decorators_test/timeout_test.py
+++ test/decorators_test/timeout_test.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 from decorated.decorators.timeout import Timeout, TimeoutError, TimeoutDecorator
 from unittest.case import TestCase
+import signal
 import time
 
 class TimeoutTest(TestCase):
     def test_within_timeout(self):
         with Timeout(10):
             pass
+        self.assertEquals(signal.SIG_DFL, signal.getsignal(signal.SIGALRM))
         
     def test_exceed_timeout(self):
         with self.assertRaises(TimeoutError):
             with Timeout(1):
                 time.sleep(10)
+        self.assertEquals(signal.SIG_DFL, signal.getsignal(signal.SIGALRM))
         
     def test_no_timeout(self):
         with Timeout(0):
             time.sleep(0.01)
+        self.assertEquals(signal.SIG_DFL, signal.getsignal(signal.SIGALRM))
             
     def test_multi_alarms(self):
         with self.assertRaises(TimeoutError):
@@ -24,7 +28,8 @@ class TimeoutTest(TestCase):
                     with Timeout(1):
                         time.sleep(10)
                 time.sleep(10)
-            
+        self.assertEquals(signal.SIG_DFL, signal.getsignal(signal.SIGALRM))
+        
     def test_outer_alarm_overdue(self):
         with self.assertRaises(TimeoutError):
             with Timeout(1):
@@ -32,6 +37,7 @@ class TimeoutTest(TestCase):
                     with Timeout(2):
                         time.sleep(10)
                 time.sleep(10)
+        self.assertEquals(signal.SIG_DFL, signal.getsignal(signal.SIGALRM))
                 
 class TimeoutDecoratorTest(TestCase):
     def test(self):
