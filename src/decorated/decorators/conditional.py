c1375331026e7d350a37b43a11a1cdfd91912165
CooledCoffee <CooledCoffee@gmail.com>
2014-04-18 15:29:41 +0800

    refactor @conditional

--- src/decorated/decorators/conditional.py
+++ src/decorated/decorators/conditional.py
@@ -5,23 +5,28 @@ import six
 
 class Conditional(Function):
     def _call(self, *args, **kw):
-        if self._test_condition(*args, **kw):
+        if self._test(*args, **kw):
             return super(Conditional, self)._call(*args, **kw)
         
-    def _init(self, condition):
-        if isinstance(condition, six.string_types):
-            self._condition = condition
+    def _init(self, condition=None):
+        if hasattr(self, '_test'):
+            _test = RemoveExtraArgs(self._test)
+        elif isinstance(condition, six.string_types):
+            def _test(*args, **kw):
+                d = self._resolve_condition_args(*args, **kw)
+                return eval(condition, d)
         elif callable(condition):
-            self._condition = RemoveExtraArgs(condition)
+            condition = RemoveExtraArgs(condition)
+            def _test(*args, **kw):
+                d = self._resolve_condition_args(*args, **kw)
+                return condition(**d)
         else:
             raise Exception('Condition can only be string or callable.')
+        self._test = _test
         
-    def _test_condition(self, *args, **kw):
+    def _resolve_condition_args(self, *args, **kw):
         d = self._resolve_args(*args, **kw)
         if 'self' in d:
             d['self_'] = d.pop('self')
-        if isinstance(self._condition, six.string_types):
-            return eval(self._condition, d)
-        else:
-            return self._condition(**d)
-        
+        return d
+    
--- test/decorators_test/conditional_test.py
+++ test/decorators_test/conditional_test.py
@@ -2,7 +2,7 @@
 from decorated.decorators.conditional import Conditional
 from unittest.case import TestCase
 
-class ConditionalTest(TestCase):
+class FunctionTest(TestCase):
     def test_string(self):
         @Conditional(condition='a == 1')
         def foo(a, b):
@@ -26,12 +26,80 @@ class ConditionalTest(TestCase):
         self.assertEqual(1, foo(1, 1))
         self.assertIsNone(foo(2, 2))
         
-    def test_on_method(self):
+    def test_sub_class(self):
+        class TestConditional(Conditional):
+            def _test(self, a):
+                return a == 1
+        @TestConditional
+        def foo(a, b):
+            return a
+        self.assertEqual(1, foo(1, 1))
+        self.assertIsNone(foo(2, 2))
+        
+class MethodTest(TestCase):
+    def test_normal(self):
         class Foo(object):
             @Conditional(condition=lambda a: a == 1)
             def bar(self, a):
                 return a
         foo = Foo()
         self.assertEqual(1, foo.bar(1))
+        self.assertIsNone(foo.bar(0))
+        
+    def test_self_in_string(self):
+        class Foo(object):
+            def __init__(self, condition):
+                self._condition = condition
+                
+            @Conditional(condition='self_._condition')
+            def bar(self, a):
+                return a
+        foo = Foo(True)
+        self.assertEqual(1, foo.bar(1))
+        foo = Foo(False)
+        self.assertIsNone(foo.bar(0))
+        
+    def test_self_in_lambda(self):
+        class Foo(object):
+            def __init__(self, condition):
+                self._condition = condition
+                
+            @Conditional(condition=lambda self_: self_._condition)
+            def bar(self, a):
+                return a
+        foo = Foo(True)
+        self.assertEqual(1, foo.bar(1))
+        foo = Foo(False)
+        self.assertIsNone(foo.bar(0))
+        
+    def test_self_in_function(self):
+        def _condition(self_):
+            return self_._condition
+        class Foo(object):
+            def __init__(self, condition):
+                self._condition = condition
+                
+            @Conditional(condition=_condition)
+            def bar(self, a):
+                return a
+        foo = Foo(True)
+        self.assertEqual(1, foo.bar(1))
+        foo = Foo(False)
+        self.assertIsNone(foo.bar(0))
+        
+    def test_self_in_sub_class(self):
+        class TestConditional(Conditional):
+            def _test(self, self_):
+                return self_._condition
+        class Foo(object):
+            def __init__(self, condition):
+                self._condition = condition
+                
+            @TestConditional
+            def bar(self, a):
+                return a
+        foo = Foo(True)
+        self.assertEqual(1, foo.bar(1))
+        foo = Foo(False)
         self.assertIsNone(foo.bar(0))
         
