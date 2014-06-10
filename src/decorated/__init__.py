a29c76752e0b2d62ce52a11118e392174698b4b6
CooledCoffee <cooledcoffee@gmail.com>
2014-06-10 10:51:58 +0800

    export wrapper function & context function in package

--- src/decorated/__init__.py
+++ src/decorated/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from decorated.base.function import Function, partial
+from decorated.base.function import Function, partial, WrapperFunction, \
+    ContextFunction
 from decorated.decorators.conditional import Conditional
 from decorated.decorators.events import Event
 from decorated.decorators.instantiate import Instantiate
@@ -10,6 +11,8 @@ from decorated.decorators.synchronized import Synchronized
 from decorated.decorators.timeout import Timeout
 
 Function = Function
+WrapperFunction = WrapperFunction
+ContextFunction = ContextFunction
 partial = partial
 
 Event = Event
