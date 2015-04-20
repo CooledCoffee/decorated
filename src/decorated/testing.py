5d0931c3a7a9ec9e7e53f001b655b0161fae4228
CooledCoffee <cooledcoffee@gmail.com>
2015-04-20 15:15:38 +0800

    fix decorated fixture

--- src/decorated/testing.py
+++ src/decorated/testing.py
@@ -3,8 +3,10 @@ from fixtures2 import PatchesFixture
 
 class DecoratedFixture(PatchesFixture):
     def disable(self, cls):
-        path = '.'.join([cls.__module__, cls.__name__, '_call'])
         def _call(self, *args, **kw):
-            return self._func(*args, **kw)
-        self.patch(path, _call)
+            if self._func is None:
+                return self._decorate(args[0])
+            else:
+                return self._func(*args, **kw)
+        self.patch_object(cls, '__call__', _call)
     
--- test/testing_test.py
+++ test/testing_test.py
@@ -1,18 +1,66 @@
 # -*- coding: utf-8 -*-
+from decorated.base.function import WrapperFunction
 from decorated.decorators.conditional import Conditional
+from decorated.decorators.once import Once
 from decorated.testing import DecoratedFixture
 from fixtures2 import TestCase
 
+class TestWrapperFunction(WrapperFunction):
+    called = False
+    
+    def _before(self, *args, **kw):
+        TestWrapperFunction.called = True
+        
 class DisableTest(TestCase):
-    def test(self):
+    def setUp(self):
+        super(DisableTest, self).setUp()
+        self.decorated = self.useFixture(DecoratedFixture())
+        
+    def test_with_args(self):
         # set up
-        fixture = self.useFixture(DecoratedFixture())
-        fixture.disable(Conditional)
         @Conditional('a != 0')
         def foo(a):
             return a
+        self.decorated.disable(Conditional)
         
         # test
         self.assertEqual(0, foo(0))
         self.assertEqual(1, foo(1))
+        
+    def test_no_arg(self):
+        # set up
+        DisableTest.args = []
+        @Once
+        def foo(a):
+            DisableTest.args.append(a)
+            return a
+        self.decorated.disable(Once)
+        
+        # test
+        self.assertEqual(1, foo(1))
+        self.assertEqual(1, foo(1))
+        self.assertEqual([1, 1], self.args)
+        
+    def test_method(self):
+        # set up
+        class Foo(object):
+            @Conditional('a != 0')
+            def bar(self, a):
+                return a
+        self.decorated.disable(Conditional)
+        
+        # test
+        self.assertEqual(0, Foo().bar(0))
+        self.assertEqual(1, Foo().bar(1))
+        
+    def test_wrapper_function(self):
+        # set up
+        @TestWrapperFunction
+        def foo(a):
+            return a
+        self.decorated.disable(TestWrapperFunction)
+
+        # test
+        foo(0)
+        self.assertFalse(TestWrapperFunction.called)
         
