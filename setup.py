38ff21527b23550b4fe8e0692cfb0779d63bd6aa
CooledCoffee <CooledCoffee@gmail.com>
2015-10-21 10:43:43 +0800

    version 1.6.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='decorated',
-    version='1.6.3',
+    version='1.6.4',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
