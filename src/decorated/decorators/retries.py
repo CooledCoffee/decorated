4c23f576d36e114823d6bf94ca03847f1bb74653
CooledCoffee <CooledCoffee@gmail.com>
2016-05-12 08:13:10 +0800

    log for retries

--- src/decorated/decorators/retries.py
+++ src/decorated/decorators/retries.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from decorated.base.function import Function
+import logging
 import time
+
+log = logging.getLogger(__name__)
 
 class Retries(Function):
     def _init(self, times, delay=0, error_types=(Exception,)):
@@ -18,7 +21,9 @@ class Retries(Function):
             except Exception as e:
                 times += 1
                 if times <= self._times and isinstance(e, self._error_types):
+                    log.warn('Execution failed. Will retry in %f seconds.' % self._delay)
                     time.sleep(self._delay)
                 else:
+                    log.warn('Execution failed permanently.')
                     raise
         
