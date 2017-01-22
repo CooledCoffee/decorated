62a3b61fa64892f7c149df44019537b8ad103575
CooledCoffee <CooledCoffee@gmail.com>
2017-01-22 17:15:49 +0800

    ttl for simple cache

--- src/decorated/decorators/cache.py
+++ src/decorated/decorators/cache.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8 -*-
+import time
+
 from decorated.base.function import Function
 from decorated.util import templates, modutil
 
 class CacheDecorator(Function):
     @property
     def invalidate(self):
-        return self._cache.uncache(self._key, **self._extra_vars)
+        return self._cache.uncache(self._key, **self._vars)
     
     def _call(self, *args, **kw):
-        d = dict(self._extra_vars)
+        d = dict(self._vars)
         d.update(self._resolve_args(*args, **kw))
         key = self._key_template.eval(d)
         return self._process(key, *args, **kw)
         
     def _decorate(self, func):
         super(CacheDecorator, self)._decorate(func)
-        var_names = self.params + tuple(self._extra_vars.keys())
+        var_names = self.params + tuple(self._vars.keys())
         self._key_template = templates.compile(self._key, var_names)
         return self
 
-    def _init(self, cache, key, extra_vars=None):
+    def _init(self, cache, key, vars=None, options=None):
         super(CacheDecorator, self)._init()
         self._cache = cache
         self._key = key
-        self._extra_vars = extra_vars or {}
+        self._vars = vars or {}
+        self._options = options
         
     def _process(self, key, *args, **kw):
         result = self._cache._get(key)
@@ -33,23 +36,23 @@ class CacheDecorator(Function):
         return result
     
 class Cache(object):
-    def cache(self, key, **kw):
+    def cache(self, key, vars=None, **options):
         class _Decorator(CacheDecorator):
             def _process(self, key, *args, **kw):
-                result = self._cache._get(key)
+                result = self._cache._get(key, options)
                 if result is None:
                     result = Function._call(self, *args, **kw)
-                    self._cache._set(key, result)
+                    self._cache._set(key, result, options)
                 return result
-        return _Decorator(self, key, extra_vars=kw)
+        return _Decorator(self, key, vars=vars, options=options)
     
-    def uncache(self, key, **kw):
+    def uncache(self, key, vars=None, **options):
         class _Decorator(CacheDecorator):
             def _process(self, key, *args, **kw):
                 result = Function._call(self, *args, **kw)
-                self._cache._delete(key)
+                self._cache._delete(key, options)
                 return result
-        return _Decorator(self, key, extra_vars=kw)
+        return _Decorator(self, key, vars=vars, options=options)
     
     def _delete(self, key):
         raise NotImplementedError()
@@ -64,14 +67,19 @@ class SimpleCache(Cache):
     def __init__(self):
         self._data = {}
         
-    def _delete(self, key):
+    def _delete(self, key, options):
         del self._data[key]
         
-    def _get(self, key):
-        return self._data.get(key)
+    def _get(self, key, options):
+        value, expires = self._data.get(key, (None, None))
+        if expires is None or expires < time.time():
+            return None
+        else:
+            return value
         
-    def _set(self, key, value):
-        self._data[key] = value
+    def _set(self, key, value, options):
+        expires = time.time() + options.get('ttl', 3600)
+        self._data[key] = (value, expires)
         
 if modutil.module_exists('pylru'):
     from pylru import lrucache
@@ -80,15 +88,15 @@ if modutil.module_exists('pylru'):
         def __init__(self, size=1000):
             self._cache = lrucache(size)
             
-        def _delete(self, key):
+        def _delete(self, key, options):
             del self._cache[key]
         
-        def _get(self, key):
+        def _get(self, key, options):
             try:
                 return self._cache[key]
             except KeyError:
                 return None
         
-        def _set(self, key, value):
+        def _set(self, key, value, options):
             self._cache[key] = value
             
--- test/decorators_test/cache_test.py
+++ test/decorators_test/cache_test.py
@@ -6,71 +6,91 @@ from unittest.case import TestCase
 class SimpleCacheTest(TestCase):
     def test(self):
         cache = SimpleCache()
-        self.assertIsNone(cache._get('a'))
-        cache._set('a', 1)
-        self.assertEquals(1, cache._get('a'))
-        cache._delete('a')
-        self.assertIsNone(cache._get('a'))
+        self.assertIsNone(cache._get('a', {}))
+        cache._set('a', 1, {})
+        self.assertEquals(1, cache._get('a', {}))
+        cache._delete('a', {})
+        self.assertIsNone(cache._get('a', {}))
 
 class LruCacheTest(TestCase):
     def test(self):
         cache = LruCache()
-        self.assertIsNone(cache._get('a'))
-        cache._set('a', 1)
-        self.assertEquals(1, cache._get('a'))
-        cache._delete('a')
-        self.assertIsNone(cache._get('a'))
+        self.assertIsNone(cache._get('a', {}))
+        cache._set('a', 1, {})
+        self.assertEquals(1, cache._get('a', {}))
+        cache._delete('a', {})
+        self.assertIsNone(cache._get('a', {}))
         
 class CacheTest(TestCase):
+    def setUp(self):
+        super(CacheTest, self).setUp()
+        self.cache = SimpleCache()
+        self.calls = 0
+
     def test_simple(self):
         # set up
-        cache = SimpleCache()
-        @cache.cache('/{id}')
+        @self.cache.cache('/{id}')
         def foo(id):
+            self.calls += 1
             return id
-        @cache.uncache('/{id}')
+        @self.cache.uncache('/{id}')
         def unfoo(id):
             return id
         
         # test
-        self.assertEqual(0, len(cache._data))
+        self.assertEqual(0, len(self.cache._data))
         
         result = foo(1)
         self.assertEqual(1, result)
-        self.assertEqual(1, len(cache._data))
+        self.assertEqual(1, self.calls)
+        self.assertEqual(1, len(self.cache._data))
         
         self.assertEqual(1, foo(1))
-        self.assertEqual(1, len(cache._data))
+        self.assertEqual(1, self.calls)
+        self.assertEqual(1, len(self.cache._data))
         
         result = unfoo(1)
         self.assertEqual(1, result)
-        self.assertEqual(0, len(cache._data))
+        self.assertEqual(0, len(self.cache._data))
         
         self.assertEqual(1, foo(1))
-        self.assertEqual(1, len(cache._data))
+        self.assertEqual(2, self.calls)
+        self.assertEqual(1, len(self.cache._data))
         
         self.assertEqual(2, foo(2))
-        self.assertEqual(2, len(cache._data))
+        self.assertEqual(3, self.calls)
+        self.assertEqual(2, len(self.cache._data))
+
+    def test_ttl(self):
+        # set up
+        @self.cache.cache('/{id}', ttl=-1)
+        def foo(id):
+            self.calls += 1
+            return id
+
+        # test
+        foo(1)
+        self.assertEqual(1, self.calls)
+        foo(1)
+        self.assertEqual(2, self.calls)
         
     def test_extra_vars(self):
         # set up
-        cache = SimpleCache()
-        @cache.cache('/{a}/{ctx.b}', a=1, ctx=ctx)
+        @self.cache.cache('/{a}/{ctx.b}', vars={'a': 1, 'ctx': ctx})
         def foo():
             pass
-        @cache.uncache('/{a}/{ctx.b}', a=1, ctx=ctx)
+        @self.cache.uncache('/{a}/{ctx.b}', vars={'a': 1, 'ctx': ctx})
         def unfoo():
             pass
         with Context(b=2):
             foo()
-            self.assertIn('/1/2', cache._data)
+            self.assertIn('/1/2', self.cache._data)
             unfoo()
-            self.assertNotIn('/1/2', cache._data)
+            self.assertNotIn('/1/2', self.cache._data)
         
     def test_invalidate(self):
         # set up
-        cache = SimpleCache()
-        foo_cache = cache.cache('/{id}')
+        foo_cache = self.cache.cache('/{id}')
         @foo_cache
         def foo(id):
             pass
@@ -79,11 +99,11 @@ class CacheTest(TestCase):
             pass
         
         # test
-        self.assertEqual(0, len(cache._data))
+        self.assertEqual(0, len(self.cache._data))
         
         foo(1)
-        self.assertEqual(1, len(cache._data))
+        self.assertEqual(1, len(self.cache._data))
         
         unfoo(1)
-        self.assertEqual(0, len(cache._data))
+        self.assertEqual(0, len(self.cache._data))
         
