ff7411489e3c5ceac4d5e4369758b991ef9dba9b
CooledCoffee <CooledCoffee@gmail.com>
2018-05-02 15:35:45 +0800

    version 1.9.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='decorated',
-    version='1.9.8',
+    version='1.9.9',
     author='Mengchen LEE',
     classifiers = [
         'Development Status :: 4 - Beta',
